3d185cf2b70b8fc453afc765ab90bc25ae0d48d3
senileteriyaki <rvusmail@gmail.com>
2026-09-07 11:26:45 -0700

    switch graphs

--- adaptiveprediction.py
+++ adaptiveprediction.py
@@ -17,8 +17,8 @@ M = 2000 #test
 n = 3000
 alpha = 0.05
 
-model = classifier1.Net()
-model.load_state_dict(torch.load("models/mnist_bad.pth"))
+model = classifier2.Net()
+model.load_state_dict(torch.load("models/mnist_good.pth"))
 model.eval()
 
 data = MNIST(
